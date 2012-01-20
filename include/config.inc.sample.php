51bb586a1c2cb98fc632271b2f12ea899ef555cc
Albrecht Oster <a.oster@online.de>
2012-01-20 01:52:51 +0100

    first commit with basic file structure

---
+++ include/config.inc.sample.php
@@ -0,0 +1,18 @@
+<?php
+// main config
+$_config = array(
+
+	// connection and authentication config for database
+	"db" => array(
+		"host"	=>	"",
+		"name"	=>	"",
+		"user"	=>	"",
+		"pass"	=>	""
+	),
+
+	"pass" => "",
+	
+	"dev" => false
+
+);
+?>
